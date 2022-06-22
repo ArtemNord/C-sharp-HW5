1f891c1709a5b33f30224fea28eca8c9972cc46d
Naganishe <mr.nordakov@mail.ru>
2022-06-22 23:09:15 +0300

    add C#HW1

---
+++ C# HW1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами.
+// Напишите программу, которая покажет колличество четных чисел в массиве.
+
+int [] CreatArray()
+{
+    int [] array = new int [10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next (100, 1000);
+        Console.Write($" {array[i]}");
+    }
+    return array;
+}
+int [] arr = CreatArray();
+
+// Console.WriteLine("Укажите длину массива: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+void PrintEvenNumbers(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            Console.WriteLine($"{i} = {array[i]}");
+        }
+    }
+}
+
+PrintEvenNumbers(arr);
