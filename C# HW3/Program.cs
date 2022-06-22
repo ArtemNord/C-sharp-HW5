c79838c226e0d81d7a035258de695af2e62aca99
Naganishe <mr.nordakov@mail.ru>
2022-06-23 00:39:18 +0300

    add C#HW3

---
+++ C# HW3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+int [] CreatArray()
+{
+    int [] array = new int [10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next (-10, 11);
+        Console.Write($" {array[i]}");
+    }
+    return array;
+}
+int [] arr = CreatArray();
+Console.WriteLine();
+
+void PrintMaxAndMinArrayElements(int [] array)
+{
+    int max = array[0];
+    int min = array[0];
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+            
+    }
+    Console.WriteLine($"Max = {max}");
+    Console.WriteLine($"Min = {min}");
+
+    
+}
+
+PrintMaxAndMinArrayElements(arr);
